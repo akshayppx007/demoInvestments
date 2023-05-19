a756f2a1cb5236e5242eca4225a0283c3640aca7
akshayppx007 <akshayppx007@gmail.com>
2023-05-19 12:10:25 +0530

    fifth commit

--- server/app.js
+++ server/app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const path = require("path");
+const helmet = require("helmet");
 
 const errorMiddleware = require("./middlewares/error");
 
 const app = express();
 
+app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,7 +18,7 @@ app.use(express.static(path.join(__dirname, "../client/dist")));
 
 // import all routes
 const user = require("./routes/userRoutes");
-const investment = require("./routes/investMentRoutes");
+const investment = require("./routes/investmentRoutes");
 
 app.use("/api/v1", user);
 app.use("/api/v1", investment);
